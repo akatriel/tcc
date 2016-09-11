62c7660074daf7aded3d9cdb4b11739e82929a06
akatriel <michael.c.gabay@gmail.com>
2016-09-11 14:27:25 +0300

    updated carousel scraper and changed ruby version in gemfile.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "http://rubygems.org/"
+ruby '2.2.5'
 gem 'pry'
 gem 'rerun'
 
--- public/scripts/scraper.rb
+++ public/scripts/scraper.rb
@@ -3,31 +3,43 @@ require 'nokogiri'
 require 'json'
 
 #http response
-page = HTTParty.get 'http://www.verizonwireless.com'
-#Make Nokogiri object
-parse_page = Nokogiri::HTML(page)
 
-images = []
-makers = []
-models = []
+	page = HTTParty.get 'http://www.verizonwireless.com', verify: false
+	#Make Nokogiri object
+	parse_page = Nokogiri::HTML(page)
 
-#parse phone carousel data
-image_nodes = parse_page.css('.phone-carousel').css('.phone-carousel-slide-wrapper')
-#grab srcs by traversing through nodes pushes them to array images
-image_nodes.each{|node| images.push node.children.children[1].attributes['src'].to_s}
+	images = []
+	makers = []
+	models = []
 
-#grabs maker and model
-text_nodes = parse_page.css('.phone-carousel').css('.phone-carousel-slide-wrapper').children.children.css('a > span')
-text_nodes.each_with_index.map{|node, index| index % 2 == 0 ? makers << node.text : models << node.text}
+	#parse phone carousel data
+	@image_nodes = parse_page.css('#category1  .phone-carousel-slide-wrapper img')
 
-## format textfile 1)img, 2)title, 3)subtitle
-package = []
-images.each_with_index do |image, index|
-	package << image
-	package << makers[index]
-	package << models[index]
-end
 
-File.open("phone_images.txt", "w+") do |f|
-	package.map{ |line| f.puts(line) }
-end
+	#grab srcs by traversing through nodes pushes them to array images
+	@image_nodes.each{|node| images.push node.attributes['src'].to_s}
+
+	#grabs maker and model
+	@text_nodes = parse_page.css('#category1').css('.phone-carousel-slide-wrapper h5 a')
+	@text_nodes.each do |node|
+		makers << node.children.css('span').first.text
+		models << node.children.css('span').last.text		
+	end
+
+	# @text_nodes.each{|node| node = node.text.split(' '); makers << node[0]; models << (node[1] << ' ' << node[2])}
+
+	# @text_nodes.each_with_index.map{|node, index| index % 2 == 0 ? makers << node.text : models << node.text}
+
+	# format textfile 1)img, 2)title, 3)subtitle
+	package = []
+	images.each_with_index do |image, index|
+		package << image
+		package << makers[index]
+		package << models[index]
+	end
+	package.each{|pa| puts pa}
+
+	# write phone_images.txt in root
+	File.open("../../phone_images.txt", "w+") do |f|
+		package.map{ |line| f.puts(line) }
+	end
